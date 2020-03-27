8fd8d99a8af973af9c1b1990a05b24af3cafa6d5
dwight-gabucan <dwight.gabucan@outlook.com>
2020-03-27 14:24:12 +0800

    sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -8,7 +8,7 @@ app.secret_key = "12345"
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", codes=session.keys())
 
 
 @app.route('/your-url', methods=['GET', 'POST'])
@@ -34,6 +34,8 @@ def your_url():
         
         with open("urls.json","w") as urls_file:
             json.dump(urls, urls_file)
+            session[request.form['code']] = True
+        
         return render_template("your_url.html", code=request.form['code'])
     else:
         return redirect(url_for("home"))
