5e69b274ff176ec3753205646ce8ef5970bda9d8
dwight-gabucan <dwight.gabucan@outlook.com>
2020-03-27 13:18:20 +0800

    files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = "12345"
@@ -20,10 +21,17 @@ def your_url():
                 urls = json.load(urls_file)
         
         if request.form["code"] in urls.keys():
-            flash("That is laready taken")
+            flash("That is already taken")
             return redirect(url_for("home"))
 
-        urls[request.form['code']] = {"url":request.form["url"]}
+        if "url" in request.form.keys():
+            urls[request.form['code']] = {"url":request.form["url"]} 
+        else:
+            f = request.files["file"]
+            full_name = request.form["code"] + secure_filename(f.filename) 
+            f.save("./" + full_name)
+            urls[request.form['code']] = {"file":full_name]}       
+        
         with open("urls.json","w") as urls_file:
             json.dump(urls, urls_file)
         return render_template("your_url.html", code=request.form['code'])
