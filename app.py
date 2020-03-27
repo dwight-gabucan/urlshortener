a90b4559058cf338d03bb45fdd86ea50da46a99c
dwight-gabucan <dwight.gabucan@outlook.com>
2020-03-27 13:53:01 +0800

    redirect file

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def your_url():
         else:
             f = request.files["file"]
             full_name = request.form["code"] + secure_filename(f.filename) 
-            f.save("./" + full_name)
+            f.save("./static/user_file/" + full_name)
             urls[request.form['code']] = {"file":full_name}       
         
         with open("urls.json","w") as urls_file:
@@ -46,4 +46,5 @@ def redirect_to_url(code):
             if code in urls.keys():
                 if "url" in urls[code].keys():
                     return redirect(urls[code]["url"])
-                
+                else:
+                    return redirect(url_for("static",filename="user_files/" + urls[code["file"]]))
